fcc4e9444d232b2e27dcda95182700b316bc2fe9
Yusuke Wada <yusukebe@cpan.org>
2010-03-08 22:53:33 +0900

    add Pod text!

--- lib/Podder/View/File.pm
+++ lib/Podder/View/File.pm
@@ -1,6 +1,7 @@
 package Podder::View::File;
 use Mouse;
 use MouseX::Types::Path::Class;
+use Pod::Simple::XHTML;
 use Text::VimColor;
 with 'Podder::View';
 use Carp;
@@ -14,10 +15,12 @@ sub BUILDARGS {
 }
 
 sub render {
-    my $self = shift;
+    my $self    = shift;
     my $content = $self->file->slurp();
-    $content = $self->highlight( $content );
-    $self->render_tt( 'file.tt2', { content => $content, title => $self->file->relative } );
+    $content = $self->highlight($content);
+    my $pod = $self->pod();
+    $self->render_tt( 'file.tt2',
+        { content => $content, title => $self->file->relative, pod => $pod } );
 }
 
 sub highlight {
@@ -27,6 +30,18 @@ sub highlight {
         filetype => 'perl', #xxx
     );
     return $syntax->html;
+}
+
+sub pod {
+    my ( $self, $text ) = @_;
+    my $parser = Pod::Simple::XHTML->new();
+    my $body;
+    $parser->output_string( \$body );
+    $parser->html_header('');
+    $parser->html_footer('');
+    $parser->html_h_level(2);
+    $parser->parse_file( $self->file->stringify );
+    return $body;
 }
 
 no Mouse;
